064e83121178ebd1d4deebbe9810b69d46a29ed4
Fagner Santiago <fagner.santiago@hotmail.com>
2021-11-29 22:14:19 -0300

    criação de teste CreateClient

---
+++ src/modules/client/repository/inMemory/CreateClientInMemory.ts
@@ -0,0 +1,60 @@
+import Client from "../../infra/typeorm/entities/Client";
+import IClientRepository from "../IClientRepository";
+
+class CreateClientInMemory implements IClientRepository {
+  private clientRepoitory: Client[] = [];
+
+  async create({
+    full_name,
+    gender,
+    birth_date,
+    age,
+    city_id,
+    id,
+  }): Promise<Client> {
+    const client = new Client();
+
+    Object.assign(client, {
+      full_name,
+      gender,
+      birth_date,
+      age,
+      city_id,
+      id,
+    });
+
+    this.clientRepoitory.push(client);
+
+    return client;
+  }
+
+  async findByName(full_name: string): Promise<Client> {
+    const clientName = this.clientRepoitory.find(
+      (find) => find.full_name === full_name
+    );
+
+    return clientName;
+  }
+
+  async findById(id: string): Promise<Client> {
+    const clientid = this.clientRepoitory.find((find) => find.id === id);
+    return clientid;
+  }
+
+  async update(id: string, full_name: string): Promise<Client> {
+    const clientUpdated = this.clientRepoitory.find((find) => find.id === id);
+    Object.assign(clientUpdated, {
+      id,
+      full_name,
+    });
+
+    this.clientRepoitory.push(clientUpdated);
+
+    return clientUpdated;
+  }
+  async delete(id: string): Promise<void> {
+    this.clientRepoitory.find((find) => find.id === id);
+  }
+}
+
+export default CreateClientInMemory;
---
+++ src/modules/client/useCase/createClient/CreateClientUseCase.spec.ts
@@ -0,0 +1,63 @@
+import AppError from "../../../../shared/Errors/AppError";
+import CreateClientInMemory from "../../repository/inMemory/CreateClientInMemory";
+import CreateClientUseCase from "./CreateClientUseCase";
+
+let clientUseCase: CreateClientUseCase;
+let createClientInMemory: CreateClientInMemory;
+
+describe("Create Client", () => {
+  beforeEach(() => {
+    createClientInMemory = new CreateClientInMemory();
+    clientUseCase = new CreateClientUseCase(createClientInMemory);
+  });
+
+  it("should be create a new Client", async () => {
+    const client = {
+      full_name: "Client Test",
+      gender: "Male",
+      birth_date: new Date(),
+      age: "33",
+      city_id: "Test's client",
+    };
+
+    await clientUseCase.execute({
+      full_name: client.full_name,
+      gender: client.gender,
+      birth_date: client.birth_date,
+      age: client.age,
+      city_id: client.city_id,
+    });
+
+    const clientCreated = await createClientInMemory.findByName(
+      client.full_name
+    );
+    expect(clientCreated).toHaveProperty("id");
+  });
+
+  it("Should not create a new client with same full_name", () => {
+    expect(async () => {
+      const client = {
+        full_name: "Client Test",
+        gender: "Male",
+        birth_date: new Date(),
+        age: "33",
+        city_id: "Test's client",
+      };
+
+      await clientUseCase.execute({
+        full_name: client.full_name,
+        gender: client.gender,
+        birth_date: client.birth_date,
+        age: client.age,
+        city_id: client.city_id,
+      });
+      await clientUseCase.execute({
+        full_name: client.full_name,
+        gender: client.gender,
+        birth_date: client.birth_date,
+        age: client.age,
+        city_id: client.city_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
