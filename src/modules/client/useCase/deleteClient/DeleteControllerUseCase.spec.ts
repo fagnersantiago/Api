2b926e22b5a66a533bde652f0df891aad55b40e6
Fagner Santiago <fagner.santiago@hotmail.com>
2022-01-13 18:57:41 -0300

    test deleção de usuário corrigido

--- src/modules/client/useCase/deleteClient/DeleteControllerUseCase.spec.ts
+++ src/modules/client/useCase/deleteClient/DeleteControllerUseCase.spec.ts
@@ -1,18 +1,39 @@
+import ClientRepository from "../../repository/ClientRepository";
+import IClientRepository from "../../repository/IClientRepository";
 import CreateClientInMemory from "../../repository/inMemory/CreateClientInMemory";
 import DeleteClientUseCase from "./DeleteClientUseCase";
+import CreateClientUseCase from "../createClient/CreateClientUseCase";
+import AppError from "../../../../shared/Errors/AppError";
 
-let deleteClientUseCase: DeleteClientUseCase;
-let clientInMemory: CreateClientInMemory;
+let createClientUseCase: CreateClientUseCase;
+let clientCreateInMemory: CreateClientInMemory;
 
 describe("Delete Client", () => {
   beforeEach(() => {
-    clientInMemory = new CreateClientInMemory();
-    deleteClientUseCase = new DeleteClientUseCase(clientInMemory);
+    clientCreateInMemory = new CreateClientInMemory();
+    createClientUseCase = new CreateClientUseCase(clientCreateInMemory);
   });
 
-  it("should be delete client", async () => {
-    const clientId = deleteClientUseCase.execute("id");
+  it("should be able to delete client", async () => {
+    expect(async () => {
+      const client = {
+        full_name: "Client Test",
+        gender: "Male",
+        birth_date: new Date(),
+        age: "33",
+        city_id: "Test's client",
+      };
 
-    expect(clientId).toBe(true);
+      const user = await createClientUseCase.execute({
+        full_name: client.full_name,
+        gender: client.gender,
+        birth_date: client.birth_date,
+        age: client.age,
+        city_id: client.city_id,
+      });
+
+      await clientCreateInMemory.delete(user.id);
+      expect(user).toBe({});
+    });
   });
 });
